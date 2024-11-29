a12f3d01cc503ccf5979bb2499931842f78db554
Marina2447 <marinaalmeida887@gmail.com>
2024-11-28 21:23:00 -0300

    atualizaçao

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
+// const meuObjLista = new XMLHttpRequest();
+// meuObjLista.open("GET", "https://raw.githubusercontent.com/DS-Senai/json/refs/heads/main/tecnologias.json");
+// meuObjLista.responseType = "json";
+// meuObjLista.send();
+
+// meuObjLista.onload  = () => {
+//     let texto = document.querySelector('.json');
+//     var tec = meuObjLista.response;
+    
+//    tec.tecnologias.forEach((objeto) => {
+//    texto.innerHTML += `
+//             ${objeto.nome} </br> 
+//             ${objeto.tipo}</br>`
+//     // document.write<hr>
+//    })
+   
+// };
+
+
 const meuObjLista = new XMLHttpRequest();
-meuObjLista.open("GET", "https://raw.githubusercontent.com/DS-Senai/json/refs/heads/main/tecnologias.json");
+meuObjLista.open("GET", "https://raw.githubusercontent.com/miqueiassousa/json/refs/heads/main/tecnologias.json");
 meuObjLista.responseType = "json";
 meuObjLista.send();
 
-meuObjLista.onload  = () => {
-    let texto = document.querySelector('.json');
+const corpo = document.querySelector("body");
+const davi = corpo.querySelector(".davi")
+
+meuObjLista.onload = () => {
     var tec = meuObjLista.response;
-    
-   tec.tecnologias.forEach((objeto) => {
-   texto.innerHTML += `
-            ${objeto.nome} </br> 
-            ${objeto.tipo}</br>`
-    // document.write<hr>
-   })
-   
-};
+    // console.log(tec)
+
+    tec.tecnologias.forEach((tecnologia) => {
+
+        for (let chave in tecnologia) {
+
+            if (chave != "bibliotecas_populares") {
+
+                davi.innerHTML += `${chave}: ${tecnologia[chave]} <br>`
+            } else {
+                tecnologia.bibliotecas_populares.forEach((bibliotecas) => {
+                    console.log(bibliotecas)
+                    for (let chave_biblioteca in bibliotecas) {
+                        davi.innerHTML += `
+                         <ul> ${chave_biblioteca}: ${bibliotecas[chave_biblioteca]} </ul>`
+                    }
+                })
+            }
+        }
+    })
+}
 
 
 
